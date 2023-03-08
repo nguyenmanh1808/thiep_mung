00d3dd2691e5c02c1ce84c0298f760e403625dd2
unknown <nguyenducmanht12@gmail.com>
2023-03-08 16:08:43 +0700

    Manh

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Quỳnh';
+const giftUrl = 'https://www.facebook.com/quynh3110';
+const eventName = 'Chúc Mừng 8 -3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc bạn có một ngày mùng 8-3 vui vẻ, càng ngày càng xinh đẹp, học giỏi  và sớm có n"gười yêu" !!! Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh_1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
